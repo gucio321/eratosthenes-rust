f5e92cfc5b1d10337c000f9694946f6fd01005de
gucio321 <73652197+gucio321@users.noreply.github.com>
2022-06-01 19:18:33 +0200

    main: add some for loop

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ use scanln::scanln;
 
 fn main() {
     let max_number = scanln!("Enter destiny number: ");
-    println!("{}", max_number)
+    let max_number_int = max_number.parse::<i32>().unwrap();
+    // for some reasons, if 5, 1..max_number_int is 4 1-4 xD
+    let mut list: Vec<i32> = (1..max_number_int+1).collect();
+    for i in list {
+        println!("{}", i);
+    }
 }
