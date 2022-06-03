9a245d19d44ce0c5bc42bbd4ce3d26b873c8adbb
gucio321 <gucio321@protonmail.com>
2022-06-03 11:10:54 +0200

    main: use list.len instead predeclared variable

--- src/main.rs
+++ src/main.rs
@@ -13,23 +13,23 @@ fn get_prime_numbers(max:i32) -> Vec<i32> {
     let mut list: Vec<i32> = (2..max+1).collect();
     let last_index = *list.last().expect("no last index?");
     let mut current_index = 0;
-    let mut list_len= list.len();
     loop {
         // search for multiplies
         let multiple = list[current_index];
         if multiple*2 > last_index {
             break;
         }
+        
         let mut i = current_index + 1;
+        
         loop {
-            if i >= list_len {
+            if i >= list.len() {
                 break;
             }
             
             let current_idx = list.get(i).expect("list is empty");
             if *current_idx != multiple && current_idx % multiple == 0 {
                 list.remove(i);
-                list_len -= 1;
             }
             
             i += 1;
